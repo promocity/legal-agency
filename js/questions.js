7b930ad6b99787f3c260c18b6f350500497ddd8c
Илья <verst.promocity.ekb@yandex.ru>
2015-12-04 13:22:32 +0500

    404 + Статья + Вопросы/Ответы

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+      var question_row_1 = document.querySelector(".question-block:nth-of-type(3) .question-row");
+      var question_1 = document.querySelector(".question-block:nth-of-type(3) .question");
+      var question_answer_1 = document.querySelector(".question-block:nth-of-type(3) .answer");
+
+      question_row_1.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_1.classList.toggle("question-row-additional");
+        question_1.classList.toggle("modal-content-show");
+        question_answer_1.classList.toggle("modal-content-show");
+      });
+
+
+      var question_row_2 = document.querySelector(".question-block:nth-of-type(4) .question-row");
+      var question_2 = document.querySelector(".question-block:nth-of-type(4) .question");
+      var question_answer_2 = document.querySelector(".question-block:nth-of-type(4) .answer");
+
+      question_row_2.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_2.classList.toggle("question-row-additional");
+        question_2.classList.toggle("modal-content-show");
+        question_answer_2.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_3 = document.querySelector(".question-block:nth-of-type(5) .question-row");
+      var question_3 = document.querySelector(".question-block:nth-of-type(5) .question");
+      var question_answer_3 = document.querySelector(".question-block:nth-of-type(5) .answer");
+
+      question_row_3.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_3.classList.toggle("question-row-additional");
+        question_3.classList.toggle("modal-content-show");
+        question_answer_3.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_4 = document.querySelector(".question-block:nth-of-type(6) .question-row");
+      var question_4 = document.querySelector(".question-block:nth-of-type(6) .question");
+      var question_answer_4 = document.querySelector(".question-block:nth-of-type(6) .answer");
+
+      question_row_4.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_4.classList.toggle("question-row-additional");
+        question_4.classList.toggle("modal-content-show");
+        question_answer_4.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_5 = document.querySelector(".question-block:nth-of-type(7) .question-row");
+      var question_5 = document.querySelector(".question-block:nth-of-type(7) .question");
+      var question_answer_5 = document.querySelector(".question-block:nth-of-type(7) .answer");
+
+      question_row_5.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_5.classList.toggle("question-row-additional");
+        question_5.classList.toggle("modal-content-show");
+        question_answer_5.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_6 = document.querySelector(".question-block:nth-of-type(8) .question-row");
+      var question_6 = document.querySelector(".question-block:nth-of-type(8) .question");
+      var question_answer_6 = document.querySelector(".question-block:nth-of-type(8) .answer");
+
+      question_row_6.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_6.classList.toggle("question-row-additional");
+        question_6.classList.toggle("modal-content-show");
+        question_answer_6.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_7 = document.querySelector(".question-block:nth-of-type(9) .question-row");
+      var question_7 = document.querySelector(".question-block:nth-of-type(9) .question");
+      var question_answer_7 = document.querySelector(".question-block:nth-of-type(9) .answer");
+
+      question_row_7.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_7.classList.toggle("question-row-additional");
+        question_7.classList.toggle("modal-content-show");
+        question_answer_7.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_8 = document.querySelector(".question-block:nth-of-type(10) .question-row");
+      var question_8 = document.querySelector(".question-block:nth-of-type(10) .question");
+      var question_answer_8 = document.querySelector(".question-block:nth-of-type(10) .answer");
+
+      question_row_8.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_8.classList.toggle("question-row-additional");
+        question_8.classList.toggle("modal-content-show");
+        question_answer_8.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_9 = document.querySelector(".question-block:nth-of-type(11) .question-row");
+      var question_9 = document.querySelector(".question-block:nth-of-type(11) .question");
+      var question_answer_9 = document.querySelector(".question-block:nth-of-type(11) .answer");
+
+      question_row_9.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_9.classList.toggle("question-row-additional");
+        question_9.classList.toggle("modal-content-show");
+        question_answer_9.classList.toggle("modal-content-show");
+       });
+
+
+      var question_row_10 = document.querySelector(".question-block:nth-of-type(12) .question-row");
+      var question_10 = document.querySelector(".question-block:nth-of-type(12) .question");
+      var question_answer_10 = document.querySelector(".question-block:nth-of-type(12) .answer");
+
+      question_row_10.addEventListener("click", function(event){
+        event.preventDefault;
+        question_row_10.classList.toggle("question-row-additional");
+        question_10.classList.toggle("modal-content-show");
+        question_answer_10.classList.toggle("modal-content-show");
+       });
